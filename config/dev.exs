917ccbbe9108b987b697342421a4dc50499837d3
Daniel Rejon <rejonpardenilla@gmail.com>
2019-09-23 11:14:26 -0500

    Configure development database

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :cynara, Cynara.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "daniel",
+  password: "",
   database: "cynara_dev",
   hostname: "localhost",
   pool_size: 10
